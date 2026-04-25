66430fe75aa1e167a29d4655620c80cd5afc0bce
Samuel Leal Vega <Samuel_Leal96@outlook.com>
2026-04-25 14:54:13 -0600

    Creando layout principal autenticacion

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom";
 
 export default function LoginView() {
-    return (
-        <>
-            <div className="text-2xl">LoginView</div>
-
-            <nav>
-                <Link to="/auth/register" className="text-blue-500">
-                    No tienes una cuenta? Registrate aquí.
-                </Link>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <nav>
+        <Link to="/auth/register" className="text-blue-500">
+          No tienes una cuenta? Registrate aquí.
+        </Link>
+      </nav>
+    </>
+  );
 }
